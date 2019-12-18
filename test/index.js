7a427ba846201e7dbcf7d216f2f49a5b9282606d
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-12-17 19:44:28 -0500

    test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
-const { SHH } = require("../index");
+// const { SHH } = require("../index");
 
-const shh = new SHH();
+// const shh = new SHH();
 
-shh
-  .sendMessageAsPost({ username: "bregy", password: "malpartida" }, "http://localhost:4000/login")
-  .then(r => console.log(r));
+// shh
+//   .sendMessageAsPost({ username: "bregy", password: "malpartida" }, "http://localhost:4000/login")
+//   .then(r => console.log(r));
+
+console.log("ok");
